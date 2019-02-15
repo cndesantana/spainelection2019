fa7c1d25976d9929aca911ed6ca8e024cf1a53de
cndesantana <charles.santana@gmail.com>
2019-02-16 00:31:42 +0100

    Plotting time series of surveys using ggplot and lubridate

--- main.R
+++ main.R
@@ -7,3 +7,49 @@
 # 2019/02/15
 ##############################################
 
+library(readxl)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(reshape)
+
+survey <- read.csv("./surveys_spain_2019.csv")
+
+#convert survey to LONG format, to work with tidyverse
+survey_long <- melt(survey, 
+                    id.vars = c("Media", "Source", "ReleaseDate","FieldDate","Size"),
+                    measure.vars = c("PP", "PSOE", "UP", "Cs", "Vox", "Others.Blank"))
+names(survey_long)[6] <- "Party"
+
+p1 <- survey_long %>%
+  ggplot(aes(x = dmy(ReleaseDate), y = as.numeric(value), col = Party), size=3) +
+  geom_point(stat="identity") +
+  scale_color_manual(values = c("PP" = "cyan", 
+                                "PSOE" = "red", 
+                                "UP" = "purple",
+                                "Cs" = "orange",
+                                "Vox" = "lightgreen",
+                                "Others.Blank" = "gray"))+
+  geom_smooth(se=FALSE)+
+  xlab("Date") + ylab("Voters(%)") + ggtitle("Spain elections 2019") 
+
+png("tiemseries1.png",width=3200,height=1800,res=300)
+print(p1)
+dev.off()
+
+p2 <- survey_long %>%
+  filter(dmy(ReleaseDate) > dmy("01-10-2018"),
+  dmy(ReleaseDate) < dmy("15-02-2019")) %>%
+  ggplot(aes(x = dmy(ReleaseDate), y = as.numeric(value), col = Party), size=3) +
+  geom_point(stat="identity") +
+  scale_color_manual(values = c("PP" = "cyan", 
+                                "PSOE" = "red", 
+                                "UP" = "purple",
+                                "Cs" = "orange",
+                                "Vox" = "lightgreen",
+                                "Others.Blank" = "gray"))+
+  geom_smooth(se=FALSE)+
+  xlab("Date") + ylab("Voters(%)") + ggtitle("Spain elections 2019") 
+png("tiemseries2.png",width=3200,height=1800,res=300)
+print(p2)
+dev.off()

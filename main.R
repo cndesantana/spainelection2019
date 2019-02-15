aa6030b112487c6b27c02f02c144b4081fa0fff4
cndesantana <charles.santana@gmail.com>
2019-02-16 00:04:40 +0100

    Adding spreadsheet of political surveys updated in 2019/02/15
    Creating main.R to work with the spreadsheet

---
+++ main.R
@@ -0,0 +1,9 @@
+##############################################
+# Script to work with political surveys
+# Collaboration OneMind and DataSCOUT
+# Antonio Canepa Oneto
+# Charles Novaes de Santana
+# --------------------------------------------
+# 2019/02/15
+##############################################
+

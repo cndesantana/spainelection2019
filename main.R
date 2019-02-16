b3506e50712ab3a1a02af05d62d50b03aa3cf175
cndesantana <charles.santana@gmail.com>
2019-02-16 08:23:18 +0100

    Adding code from Daniel Marcelino's post so we can adapt it to our problems

--- main.R
+++ main.R
@@ -56,3 +56,77 @@ png("tiemseries2.png",width=3200,height=1800,res=300)
 print(p2)
 dev.off()
 
+######
+###### To run the Bayes model
+###### Inspired by http://danielmarcelino.github.io/2018/will-brazil-goes-to-a-instant-runoff-election.html
+######
+######
+
+## the survey needs to present the N
+survey_long_N <- survey_long %>% 
+  filter(Size > 0)
+
+options(digits=3)
+
+### -------- From now on, the code needs to be adjusted to our problem
+#####################Adjusting for the wasting votes
+wtd.polls[9,] <- data.frame(wtd.polls[8,1:4] +
+                              wtd.polls[8,1:4] / 
+                              sum(wtd.polls[8,1:4]) * 
+                              wtd.polls[8,6], 
+                            Swing=0, 
+                            Wasting=0, 
+                            N=(wtd.polls[8,7] - (wtd.polls[8,6] * wtd.polls[8,7])))
+
+print(wtd.polls)
+
+wtd.polls$N[9] *
+  c(wtd.polls$Bolsonaro[9], 
+    (wtd.polls$Haddad[9] + wtd.polls$Ciro[9] + wtd.polls$Others[9]), 
+    1 - wtd.polls$Bolsonaro[9] - (wtd.polls$Haddad[9] - wtd.polls$Ciro[9] - wtd.polls$Others[9]))+1
+
+
+###########Draw 1 million samples
+poll <- c(4910, 3011, 1756, 2718)
+
+library(SciencesPo)
+library(MCMCpack)
+
+############################# draw samples from the posterior
+set.seed(1234)
+MC <- 1000000
+
+### Using uninformative prior (1,1,1,1)
+#samples <- getDirichletSamples(MC, alpha = poll + rep(1,4))  
+
+row= 9
+prob2win = function(row, export=1){
+  p=rdirichlet(100000,
+               wtd.polls$N[row] *
+                 c(wtd.polls$Bolsonaro[row], wtd.polls$Haddad[row] + wtd.polls$Ciro[row] + wtd.polls$Others[row], 1 - wtd.polls$Bolsonaro[row] - wtd.polls$Haddad[row] - wtd.polls$Ciro[row] - wtd.polls$Others[row])+1)
+  if(export==1){
+    mean(p[,1]<p[,2]) ## No exceeds Yes?
+  } else {
+    return(p)
+  }
+}
+
+
+#####################look at the margins of Bolsonaro over the combined opposition candidates.
+samples = prob2win(row= 9, export=0)
+
+combinedOpposition <- (samples[,2])
+frontRunner <- (samples[,1])
+
+margin <- (combinedOpposition - frontRunner)
+
+quantile(margin, probs = c(0.025, 0.5, 0.975))
+
+hist(margin, 
+     col="gray",
+     prob = FALSE, # posterior distribution
+     breaks = "FD", xlab = expression(p[Bolsonaro] > p[Opposition]),
+     main = expression(paste(bold("Posterior Distribution of Elections With "),  p[Bolsonaro] > p[Opposition])));
+# Bayes estimate (middle 95%)
+abline(v=mean(margin), col='red', lwd=3, lty=3);
+

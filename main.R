7f9727cf8998318cac29fd54c7ad6be053a91f48
cndesantana <charles.santana@gmail.com>
2019-02-18 02:46:40 +0100

    Fixing small typo in csv file
    creating new figures
    
    creating first version of the bayesian prediction. Still needs to be tested.

--- main.R
+++ main.R
@@ -68,26 +68,26 @@ survey_N <- survey %>%
 
 options(digits=3)
 
-### -------- From now on, the code needs to be adjusted to our problem
-#####################Adjusting for the wasting votes
-wtd.polls[9,] <- data.frame(wtd.polls[8,1:4] +
-                              wtd.polls[8,1:4] / 
-                              sum(wtd.polls[8,1:4]) * 
-                              wtd.polls[8,6], 
-                            Swing=0, 
-                            Wasting=0, 
-                            N=(wtd.polls[8,7] - (wtd.polls[8,6] * wtd.polls[8,7])))
+wtd.polls <- survey_N %>% 
+  filter(dmy(ReleaseDate) > dmy("01-01-2019")) %>%
+  select(PP, PSOE, Cs, UP, Vox, Others.Blank, Size)
 
-print(wtd.polls)
-
-wtd.polls$N[9] *
-  c(wtd.polls$Bolsonaro[9], 
-    (wtd.polls$Haddad[9] + wtd.polls$Ciro[9] + wtd.polls$Others[9]), 
-    1 - wtd.polls$Bolsonaro[9] - (wtd.polls$Haddad[9] - wtd.polls$Ciro[9] - wtd.polls$Others[9]))+1
+wtd.polls[nrow(wtd.polls)+1,1:6] <- colMeans(wtd.polls[,1:6], na.rm=TRUE)  
+wtd.polls[nrow(wtd.polls)+1,7] <-  colSums(wtd.polls[,7], na.rm=TRUE)
+### -------- The following table is now adjusted to our problem
+#               Bolsonaro Haddad  Ciro   Others  Swing   Wasting  N
+#Datafolha      0.360     0.220   0.130  0.200   0.0400  0.0500   19552
+#Ibope          0.360     0.220   0.110  0.180   0.0500  0.0800   3010
+#MDA            0.367     0.240   0.099  0.155   0.0600  0.0780   2002
+#Ipespe         0.360     0.220   0.110  0.290   0.0200  0.0000   2000
+#VoxPopuli      0.340     0.270   0.110  0.130   0.0700  0.0800   2000
+#ParanaPesq     0.349     0.218   0.094  0.171   0.0460  0.1200   1080
+#7              0.359     0.225   0.122  0.195   0.0433  0.0561   29644
+#8              0.376     0.235   0.128  0.205   0.0000  0.0561   29644
+#9              0.398     0.249   0.135  0.217   0.0000  0.0000   27980
 
 
 ###########Draw 1 million samples
-poll <- c(4910, 3011, 1756, 2718)
 
 library(SciencesPo)
 library(MCMCpack)
@@ -97,13 +97,14 @@ set.seed(1234)
 MC <- 1000000
 
 ### Using uninformative prior (1,1,1,1)
-#samples <- getDirichletSamples(MC, alpha = poll + rep(1,4))  
 
-row= 9
 prob2win = function(row, export=1){
   p=rdirichlet(100000,
                wtd.polls$N[row] *
-                 c(wtd.polls$Bolsonaro[row], wtd.polls$Haddad[row] + wtd.polls$Ciro[row] + wtd.polls$Others[row], 1 - wtd.polls$Bolsonaro[row] - wtd.polls$Haddad[row] - wtd.polls$Ciro[row] - wtd.polls$Others[row])+1)
+                 c(wtd.polls$PP[row] + wtd.polls$Cs[row]+ wtd.polls$Vox[row] , 
+                   wtd.polls$PSOE[row]  + wtd.polls$UP[row] + wtd.polls$Others.Blank[row], 
+                   1 - wtd.polls$PP[row] - wtd.polls$Cs[row] - wtd.polls$Vox[row] - wtd.polls$PSOE[row] - wtd.polls$UP[row] - wtd.polls$Others.Blank[row])
+               +1)
   if(export==1){
     mean(p[,1]<p[,2]) ## No exceeds Yes?
   } else {
@@ -111,9 +112,9 @@ prob2win = function(row, export=1){
   }
 }
 
-
 #####################look at the margins of Bolsonaro over the combined opposition candidates.
-samples = prob2win(row= 9, export=0)
+row= nrow(wtd.polls)
+samples = prob2win(row= row, export=0)
 
 combinedOpposition <- (samples[,2])
 frontRunner <- (samples[,1])
@@ -125,8 +126,7 @@ quantile(margin, probs = c(0.025, 0.5, 0.975))
 hist(margin, 
      col="gray",
      prob = FALSE, # posterior distribution
-     breaks = "FD", xlab = expression(p[Bolsonaro] > p[Opposition]),
-     main = expression(paste(bold("Posterior Distribution of Elections With "),  p[Bolsonaro] > p[Opposition])));
+     breaks = "FD", xlab = expression(p[FarRight] > p[Opposition]),
+     main = expression(paste(bold("Posterior Distribution of Elections With "),  p[FarRight] > p[Opposition])));
 # Bayes estimate (middle 95%)
-abline(v=mean(margin), col='red', lwd=3, lty=3);
-
+abline(v=mean(margin), col='red', lwd=3, lty=3)
